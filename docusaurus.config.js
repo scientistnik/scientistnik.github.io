a84007ab1db1f6710e18bd4ce02ac8d905b29222
Nikolay Nozdrin-Plotnitsky <scientistnik89@gmail.com>
2023-01-02 18:59:56 +0300

    fix broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,7 +90,7 @@ const config = {
             items: [
               {
                 label: "useful",
-                to: "/docs",
+                to: "/docs/intro",
               },
               {
                 label: "Blog",
